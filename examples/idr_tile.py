ceaab3ae821b3727b0df76aecc95eea21e420d41
Kyle Harrington <czi@kyleharrington.com>
2022-11-02 12:25:35 -0400

    Add example of fetching from IDR with tiles (slow)

---
+++ examples/idr_tile.py
@@ -0,0 +1,114 @@
+import idr
+import napari
+
+import numpy as np
+import dask.array as da
+from dask import delayed
+
+# from .omero_utils import load_numpy_array_lazy, PIXEL_TYPES
+from omero_utils import load_numpy_array_lazy, PIXEL_TYPES
+
+viewer = napari.Viewer()
+
+conn = idr.connection(host='ws://idr.openmicroscopy.org/omero-ws', user='public', password='public')
+
+# image_id = 9846151
+image_id = 9822151
+
+# napari.layers.Image._is_async = lambda x: False
+
+# data = load_numpy_array(image)
+# data, meta = load_numpy_array_lazy(image_id, conn=conn)
+
+# print('data loaded', data.shape)
+
+# print('async', napari.layers.Image._is_async(None))
+
+# viewer.add_image(data)
+
+# print('image added to viewer')
+
+# Tile tests
+
+image = conn.getObject("Image", image_id)
+nt, nc, nz, ny, nx = [getattr(image, f'getSize{x}')() for x in 'TCZYX']
+
+print('get_data_lazy', [nt, nc, nz, ny, nx])
+
+x, y = 0, 0
+width, height = 10000, 10000
+    
+tile = (x, y, width, height)
+    
+pixels = image.getPrimaryPixels()
+dtype = PIXEL_TYPES.get(pixels.getPixelsType().value, None)
+
+def exception_idx(idx):
+    print('exception_idx', idx, type(idx))
+    if idx > 1:
+        raise Exception("test exception")
+    return pixels.getPlane(*idx)
+
+#get_plane = delayed(exception_idx)
+#get_plane = delayed(lambda idx: pixels.getPlane(*idx))
+
+def get_tile(x,y):
+    conn = idr.connection(host='ws://idr.openmicroscopy.org/omero-ws', user='public', password='public')
+    image = conn.getObject("Image", image_id)
+    pixels = image.getPrimaryPixels()
+    return pixels.getTile(0, 0, 0, (x, y, width, height))
+
+    
+def get_lazy_tile(x, y):
+    return da.from_delayed(get_tile(x, y), shape=(width, height), dtype=dtype)
+
+# TODO continue here
+# Make a dask array of the correct size
+# Fill each tile with the lazy tile source
+
+print('starting to fetch')
+
+# 5D stack: TCZXY
+t_stacks = []
+for t in range(nt):
+    c_stacks = []
+    for c in range(nc):
+        z_stack = []
+        for z in range(nz):
+            y_stack = []
+            for y in range(0, ny, height):
+                x_stack = []
+                # ragged border
+                if y + height > ny:
+                    pass
+                else:
+                    for x in range(0, nx, width):
+                        print((x, y, z, c, t))
+                        if x + width > nx:
+                            pass
+                        else:
+                            x_stack.append(get_lazy_tile(x, y))
+                    y_stack.append(da.concatenate(x_stack, axis=0))
+            z_stack.append(da.stack(y_stack))
+        c_stacks.append(da.stack(z_stack))
+    t_stacks.append(da.stack(c_stacks))
+
+result = da.stack(t_stacks)
+
+value = result.sum()
+print('starting to take sum')
+value.compute()
+
+print(f"sum: {value}")
+
+# tile_a = get_lazy_tile(0, 0)
+# tile_b = get_lazy_tile(0, 1000)
+
+# only works with a 2D tile
+# tiles = da.from_delayed(get_tile, dtype=dtype, shape=(width, height))
+
+# stack = da.map_blocks(get_tile, dtype=dtype, meta=np.array((), dtype=dtype))
+# stack = da.map_blocks(get_tile, dtype=dtype, meta={'dtype':dtype})
+
+if __name__ == '__main__':
+    napari.run()
