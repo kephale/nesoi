4475fb8ae8dbbea9006e230566bdf52169dee0d6
Kyle Harrington <czi@kyleharrington.com>
2022-11-04 10:48:48 -0400

    Update environment add idr_tile example

--- examples/idr_tile.py
+++ examples/idr_tile.py
@@ -45,6 +45,9 @@ print('starting to fetch')
 ny = int(ny / height) * height
 nx = int(nx / width) * width
 
+# ny = height * 2
+# nx = width * 1
+
 # 5D stack: TCZXY
 t_stacks = []
 for t in range(nt):
@@ -52,25 +55,26 @@ for t in range(nt):
     for c in range(nc):
         z_stack = []
         for z in range(nz):
-            y_stack = []
-            for y in range(0, ny, height):
-                x_stack = []
+            x_stack = []
+            for x in range(0, nx, height):
+                y_stack = []
                 # ragged border
-                if y + height > ny:
+                if x + width > nx:
                     pass
                 else:
-                    for x in range(0, nx, width):
+                    for y in range(0, ny, width):
                         print((x, y, z, c, t))
-                        if x + width > nx:
+                        if y + height > ny:
                             pass
                         else:
-                            x_stack.append(get_lazy_tile(x, y))
-                    y_stack.append(da.concatenate(x_stack, axis=0))
-            z_stack.append(da.stack(y_stack))
+                            y_stack.append(get_lazy_tile(x, y))
+                    x_stack.append(da.concatenate(y_stack, axis=0))
+            z_stack.append(da.stack(x_stack))
         c_stacks.append(da.stack(z_stack))
     t_stacks.append(da.stack(c_stacks))
 
 result = da.stack(t_stacks)
+# result.compute()
 
 # Doing this will trigger fetching all tiles
 # value = result.sum()
@@ -85,7 +89,10 @@ viewer.camera.center = (0.0, 51626.14114205004, 5029.208202210406)
 viewer.camera.zoom = 0.14077092440711514
 # viewer.camera.angles = (0.0, 0.0, 90.0), perspective=0.0, interactive=True)
 
-viewer.add_image(result)
+# viewer.add_image(result)
+
+layer = napari.layers.Image(result)
+viewer.layers.append(layer)
 
 if __name__ == '__main__':
     napari.run()
